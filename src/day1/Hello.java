b53f6085fc4b3036ab5e062782d806653fbbef62
Madina Absalyamova <amadina@yahoo.com>
2019-09-09 01:34:11 -0400

    hello git

--- src/day1/Hello.java
+++ src/day1/Hello.java
@@ -2,8 +2,7 @@ package day1;
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("Hello World");
-	System.out.println("Hello Git");
-	System.out.println(" First day using git and GitHub");
+	System.out.println("Hello github!");
+	
 }
 }
