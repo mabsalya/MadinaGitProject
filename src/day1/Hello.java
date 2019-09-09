bc3e633a65ceda22ba0e6f917489809a7b3677d1
Madina Absalyamova <amadina@yahoo.com>
2019-09-09 01:23:19 -0400

    second commit

--- src/day1/Hello.java
+++ src/day1/Hello.java
@@ -3,5 +3,7 @@ package day1;
 public class Hello {
 public static void main(String[] args) {
 	System.out.println("Hello World");
+	System.out.println("Hello Git");
+	System.out.println(" First day using git and GitHub");
 }
 }
